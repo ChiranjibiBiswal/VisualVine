ad2998008c331b088173c444f2723547c7726350
Chiranjibi Biswal <chiranjibibiswal2002@gmail.com>
2025-03-17 14:20:43 +0530

    create custom response and error handling

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # VisualVine
 
-## A visual video application designed to enhance video experiences. This project aims to provide an engaging and interactive platform for users.
+A visual video application designed to enhance video experiences. This project aims to provide an engaging and interactive platform for users.
--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
     path: './env'
 })
 
-connectDB();
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 5000, () => console.log('Listening on port ' + process.env.PORT || 5000));
+})
+.catch((err) => {
+    console.log("MongoDB Connection error", err);
+});
